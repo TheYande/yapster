df3a7f662552173d86d8dcb3813c2393ec765487
Jazza231 <greeny-231@outlook.com>
2024-07-20 14:05:38 +0800

    env

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD28R4Bqn8fxygXfOgFgUVWfst3Yg511po",
-  authDomain: "yapster-d60ee.firebaseapp.com",
-  projectId: "yapster-d60ee",
-  storageBucket: "yapster-d60ee.appspot.com",
-  messagingSenderId: "175338596069",
-  appId: "1:175338596069:web:dc41c40983e43ebe2a560c",
-  measurementId: "G-88N232T5SN",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
